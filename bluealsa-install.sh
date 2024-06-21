a0c0ff0e902d7289e4b0d5dda7074a08b394248a
Charles Buckman <buckmanc@gmail.com>
2024-06-20 20:44:00 -0500

    refinements, bluealsa install script

---
+++ bluealsa-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+git clone https://github.com/arkq/bluez-alsa.git
+cd bluez-alsa/ 
+sudo apt install -y autoconf autoconf libdbus-1-dev libasound2-dev libbluetooth-dev libtool libglib2.0-dev libdbus-1-dev libsbc-dev libmp3lame-dev
+mkdir build
+cd build
+../configure --enable-mp3lame --enable-systemd --with-bluealsauser=$USER
+make
+sudo make install
+sudo systemctl enable bluealsa.service
+sudo systemctl start bluealsa.service
+sudo systemctl enable bluealsa-aplay.service
+sudo systemctl start bluealsa-aplay.service
+
--- bluetoothctl-aggressive-pair.sh
+++ bluetoothctl-aggressive-pair.sh
@@ -22,101 +22,148 @@ touch -a "$exclusionsPath"
 
 bluetoothctl power on
 
-echo "scanning..."
-# bluetoothctl scan on | while read -r line
-# stdbuf -oL bluetoothctl scan on | while read -r line
-while read -r line
+while true
 do
-	if ! grep -iq "$line" "$logPathRaw"
+
+	echo "scanning..."
+	# bluetoothctl scan on | while read -r line
+	# stdbuf -oL bluetoothctl scan on | while read -r line
+	while read -r line
+	do
+		if ! grep -iq "$line" "$logPathRaw"
+		then
+			echo "$line"
+			echo "$line" >> "$logPathRaw"
+		fi
+
+		if [ "$line" == "Discovery started" ] || [[ "$line" == *"] Controller "* ]]
+		then
+			continue
+		fi
+
+		# strip control chars
+		line=$(echo "$line" | ansi2txt | perl -pe 's/[\cA\cB]//g') 
+		event=$(echo "$line" | grep -Pio '^\[\w+?\] ' | perl -pe 's/[ \[\]]//g')
+		logLine=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\2,\3/g')
+		deviceName=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\4/g')
+		macAddy=$(echo "$line" | grep -Pio '\w\w:\w\w:\w\w:\w\w:\w\w:\w\w')
+
+
+		bInfo=$(bluetoothctl info "$macAddy")
+		bName=$(echo "$bInfo" | grep -Pio '(?<=name: ).+?(?=     |$)')
+		bClass=$(echo "$bInfo" | grep -Pio '(?<=class: ).+?(?=     |$)')
+		bUUID=$(echo "$bInfo" | grep -Pio '(?<=UUID: ).+?(?=     |$)')
+		bIcon=$(echo "$bInfo" | grep -Pio '(?<=Icon: ).+?(?=     |$)')
+		# bIcon houses an enum-like device type
+		# what to do when missing? Are missing items BLE devices/beacons?
+
+		bPaired=$(echo "$bInfo" | grep -Pio '(?<=Paired: ).+?(?=     |$)')
+		bTrusted=$(echo "$bInfo" | grep -Pio '(?<=Trusted: ).+?(?=     |$)')
+		bConnected=$(echo "$bInfo" | grep -Pio '(?<=Connected: ).+?(?=     |$)')
+
+		# echo "macAddy: $macAddy"
+		# echo "bInfo: $bInfo"
+
+		# use the name from bluetoothctl info if present
+		if [[ -n "$bName" ]]
+		then
+			deviceName="$bName"
+		fi
+
+		logLine="$logLine,\"$deviceName\",$bIcon,$bClass,\"$bUUID"\"
+
+		if ! grep -Fiq "$logLine" "$logPath" && [[ -n "$deviceName" ]]
+		then
+			echo "$logLine" | ts "%F,%R," >> "$logPath"
+		fi
+
+		# could make this a wildcard match, but direct mac addy is probably better
+		if grep -iq "$macAddy" "$exclusionsPath"
+		then
+			echo "$deviceName is excluded, skipping"
+			continue
+		fi
+
+		borkedLogPath="$logRootPath/borked_$(date +%F_%H).log"
+		if [[ -f "$borkedLogPath" ]]
+		then
+			borkedCount="$(grep -Fic "$macAddy" "$borkedLogPath")"
+			if [[ "$borkedCount" -ge 4 ]]
+			then
+				echo "$deviceName has failed to connect repeatedly recently, skipping"
+				continue
+			fi
+		fi
+
+		loggerPath="$HOME/bin/_log"
+		if [[ -x "$loggerPath" ]] && echo "$deviceName" | grep -Piqv '(rssi|txpower): '
+		then
+			"$loggerPath" "bluetooth" "\"$deviceName\",$macAddy" > /dev/null
+		fi
+
+		# probable list of "icon" values that indicate audio output devices to pair with
+		# audio-card
+		# audio-headphones
+		# audio-headset
+		# multimedia-player
+
+		# try only pairing on "new" event for now?
+		# what about the pairing event?
+		if [[ "$bIcon" =~ ^(audio-.+|multimedia-player)$ ]] && [ "$event" == "NEW" ]
+		then
+			echo "$deviceName is an audio device!"
+			break
+		fi
+
+	done < <( stdbuf -oL bluetoothctl scan on )
+
+	connectError=0
+
+	if [[ "$bPaired" == "no" && "$connectError" == "0" ]]
 	then
-		echo "$line"
-		echo "$line" >> "$logPathRaw"
+		echo "pairing..."
+		if ! msg="$(bluetoothctl pair "$macAddy" 2>&1)"
+		then
+			connectError=1
+		fi
+
+		echo "$msg"	| ts "%F,%R," | tee -a "$logPathConnectAttempts"
 	fi
 
-	if [ "$line" == "Discovery started" ] || [[ "$line" == *"] Controller "* ]]
+	if [[ "$bTrusted" == "no" && "$connectError" == "0" ]]
 	then
-		continue
+		echo "trusting..."
+		if ! msg="$(bluetoothctl trust "$macAddy" 2>&1)"
+		then
+			connectError=1
+		fi
+
+		echo "$msg"	| ts "%F,%R," | tee -a "$logPathConnectAttempts"
 	fi
 
-	# strip control chars
-	line=$(echo "$line" | ansi2txt | perl -pe 's/[\cA\cB]//g') 
-	event=$(echo "$line" | grep -Pio '^\[\w+?\] ' | perl -pe 's/[ \[\]]//g')
-	logLine=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\2,\3/g')
-	deviceName=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\4/g')
-	macAddy=$(echo "$line" | grep -Pio '\w\w:\w\w:\w\w:\w\w:\w\w:\w\w')
-
-
-	bInfo=$(bluetoothctl info "$macAddy")
-	bName=$(echo "$bInfo" | grep -Pio '(?<=name: ).+?(?=     |$)')
-	bClass=$(echo "$bInfo" | grep -Pio '(?<=class: ).+?(?=     |$)')
-	bUUID=$(echo "$bInfo" | grep -Pio '(?<=UUID: ).+?(?=     |$)')
-	bIcon=$(echo "$bInfo" | grep -Pio '(?<=Icon: ).+?(?=     |$)')
-	# bIcon houses an enum-like device type
-	# what to do when missing? Are missing items BLE devices/beacons?
-
-	bPaired=$(echo "$bInfo" | grep -Pio '(?<=Paired: ).+?(?=     |$)')
-	bTrusted=$(echo "$bInfo" | grep -Pio '(?<=Trusted: ).+?(?=     |$)')
-	bConnected=$(echo "$bInfo" | grep -Pio '(?<=Connected: ).+?(?=     |$)')
-
-	# echo "macAddy: $macAddy"
-	# echo "bInfo: $bInfo"
-
-	# only accept device name from the "new" event of the main scan
-	# otherwise, use the one nabbed from bluetoothctl info
-	if [ "$event" != "NEW" ]
+	if [[ "$bConnected" == "no" && "$connectError" == "0" ]]
 	then
-		deviceName="$bName"
+		echo "connecting..."
+		if ! msg="$(bluetoothctl connect "$macAddy" 2>&1)"
+		then
+			connectError=1
+		fi
+
+		echo "$msg"	| ts "%F,%R," | tee -a "$logPathConnectAttempts"
 	fi
 
-	logLine="$logLine,\"$deviceName\",$bIcon,$bClass,\"$bUUID"\"
-
-	if ! grep -iq "$logLine" "$logPath" && [ -n "$deviceName" ]
+	# if there are no errors from trying to connect
+	# break out of the loop
+	# otherwise, loop back and restart scanning
+	if [[ "$connectError" == "0" ]]
 	then
-		echo "$logLine" | ts "%Y%m%d,%H%M%S," >> "$logPath"
-	fi
-
-	# check exclusions here
-
-	# probable list of "icon" values that indicate audio output devices to pair with
-	# audio-card
-	# audio-headphones
-	# audio-headset
-	# multimedia-player
-
-	# try only pairing on "new" event for now?
-	# what about the pairing event?
-	if [[ "$bIcon" =~ ^(audio-.+|multimedia-player)$ ]] && [ "$event" == "NEW" ]
-	then
-		echo "$deviceName is an audio device!"
 		break
+	else
+		echo "$macAddy" >> "$borkedLogPath"
 	fi
 
-	loggerPath="$HOME/bin/_log"
-	if [[ -x "$loggerPath" ]]
-	then
-		"$loggerPath" "$deviceName,macAddy"
-	fi
-
-done < <( stdbuf -oL bluetoothctl scan on )
-
-# TODO error handle these
-if [ "$bPaired" == "no" ]
-then
-	bluetoothctl pair "$macAddy" 2>&1 | ts "%Y%m%d,%H%M%S," | tee "$logPathConnectAttempts"
-fi
-
-if [ "$bTrusted" == "no" ]
-then
-	bluetoothctl trust "$macAddy" 2>&1 | ts "%Y%m%d,%H%M%S," | tee "$logPathConnectAttempts"
-	# some users disconnect after trust
-fi
-
-if [ "$bConnected" == "no" ]
-then
-	# bluetoothctl connect "$macAddy" || bluetoothctl pair "$macAddy"
-	bluetoothctl connect "$macAddy" 2>&1 | ts "%Y%m%d,%H%M%S," | tee "$logPathConnectAttempts"
-fi
+done
 
 # TODO better checking for if we're successfully paired
 
-echo "$logLine" | ts "%Y%m%d,%H%M%S," >> "$logPathPaired"
+echo "$logLine" | ts "%F,%R," >> "$logPathPaired"
