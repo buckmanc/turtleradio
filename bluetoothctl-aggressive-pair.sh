70a234b1b423dc0728c0ca9e084d32448a049593
Charles Buckman <buckmanc@gmail.com>
2024-06-28 23:18:11 -0500

    add log only functionality

--- bluetoothctl-aggressive-pair.sh
+++ bluetoothctl-aggressive-pair.sh
@@ -13,6 +13,32 @@ logPathPaired="$logRootPath/paired.log"
 logPathConnectAttemptMessages="$logRootPath/connect_attempt_messages.log"
 exclusionsPath="$gitRoot/exclusions.config"
 
+optLogOnly=0
+optLoop=0
+optAllDeets=0
+newRecords=0
+
+for arg in "$@"
+do
+	if [[ "$arg" == "-l" || "$arg" == "--loop" ]]
+	then
+		optLoop=1
+	elif [[ "$arg" == "-a" || "$arg" == "--all" ]]
+	then
+		optAllDeets=1
+	elif [[ "$arg" == "--log-only" ]]
+	then
+		optLogOnly=1
+	fi
+done
+
+if [[ "$optLoop" == "0" ]]
+then
+	timeoutTime=30
+else
+	timeoutTime=99999
+fi
+
 mkdir -p "$logRootPath"
 touch -a "$logPath"
 touch -a "$logPathRaw"
@@ -30,13 +56,17 @@ do
 	# stdbuf -oL bluetoothctl scan on | while read -r line
 	while read -r line
 	do
-		if ! grep -iq "$line" "$logPathRaw"
+		if [[ "$optAllDeets" == "1" ]]
 		then
 			echo "$line"
+		fi
+
+		if ! grep -iq "$line" "$logPathRaw"
+		then
 			echo "$line" >> "$logPathRaw"
 		fi
 
-		if [ "$line" == "Discovery started" ] || [[ "$line" == *"] Controller "* ]]
+		if [[ "$line" == "Discovery started" ]] || [[ "$line" == *"] Controller "* ]]
 		then
 			continue
 		fi
@@ -49,6 +79,12 @@ do
 		logLine=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\2,\3/g')
 		deviceName=$(echo "$line" | perl -pe 's/^(\S+?) (\S+?) (\S+?) (.+)$/\4/g')
 		macAddy=$(echo "$line" | grep -Pio "$macAddyRegex" | head -n 1)
+
+		# if mac addy is blank then we got something other than what we wanted
+		if [[ -z "$macAddy" ]]
+		then
+			continue
+		fi
 
 
 		bInfo=$(bluetoothctl info "$macAddy")
@@ -72,12 +108,47 @@ do
 			deviceName="$bName"
 		fi
 
+		# clean up valid names
+		deviceName="$(echo "$deviceName" | perl -pe 's/^(name|alias): ?//g')"
+
 		logLine="$logLine,\"$deviceName\",$bIcon,$bClass,\"$bUUID"\"
 
 		if ! grep -Fiq "$logLine" "$logPath" && [[ -n "$deviceName" ]]
 		then
 			echo "$logLine" | ts "%F,%R," >> "$logPath"
 		fi
+
+		if echo "$deviceName" | grep -Piq '^"?(rssi|txpower|manufacturerdata\.key|manufacturerdata\.value|uuids): ' || echo "$deviceName" | grep -Piq "$macAddyRegex"
+		then
+			deviceName=''
+		fi
+
+		# primary log
+		loggerPath="$HOME/bin/_log"
+		if [[ -x "$loggerPath" ]]
+		then
+			logResult="$("$loggerPath" "bluetooth" "\"$deviceName\",$macAddy")"
+		fi
+
+		if [[ "$optLogOnly" == "1" ]]
+		then
+			if [[ "$optAllDeets" == "0" ]]
+			then
+				# eat nopers message, prefer to display at end
+				if ! echo "$logResult" | grep -Piq "^no new"
+				then
+					echo "$logResult"
+				fi
+			fi
+
+			if ! echo "$logResult" | grep -Piq "^no new"
+			then
+				newRecords=1
+			fi
+
+			continue
+		fi
+
 
 		# could make this a wildcard match, but direct mac addy is probably better
 		if grep -iq "$macAddy" "$exclusionsPath"
@@ -97,17 +168,6 @@ do
 			fi
 		fi
 
-		if echo "$deviceName" | grep -Piq '(rssi|txpower): ' || echo "$deviceName" | grep -Piq "$macAddyRegex"
-		then
-			deviceName=''
-		fi
-
-		loggerPath="$HOME/bin/_log"
-		if [[ -x "$loggerPath" ]]
-		then
-			"$loggerPath" "bluetooth" "\"$deviceName\",$macAddy" > /dev/null
-		fi
-
 		# probable list of "icon" values that indicate audio output devices to pair with
 		# audio-card
 		# audio-headphones
@@ -122,7 +182,19 @@ do
 			break
 		fi
 
-	done < <( stdbuf -oL bluetoothctl --timeout 99999 scan on )
+	# timeout option is required for non interactive scanning
+	done < <( stdbuf -oL bluetoothctl --timeout "$timeoutTime" scan on )
+
+	# report and back out here if logging only
+	if [[ "$optLogOnly" == "1" ]]
+	then
+		if [[ "$optLogOnly" == "1" && "$newRecords" == "0" && "$optLoop" == "0" ]]
+		then
+			echo "no new devices logged"
+		fi
+
+		exit
+	fi
 
 	connectError=0
 	echo "$macAddy" >> "$logPathConnectAttempts"
